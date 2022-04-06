c1bf53c47025646f9e2054356c378257f05e51cd
vlnt <56691071+vlntpr@users.noreply.github.com>
2022-04-06 21:13:55 +0400

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+
+Route::get('/', function () { 
     return view('home');
-});
+})->name('home');
+
+Route::get('/fandoms', function () { 
+    return view('fandoms');
+})->name('fandoms');
+
+Route::get('/more', function () { 
+    return view('fandoms-more');
+})->name('more');
+
+Route::get('/works', function () { 
+    return view('works');
+})->name('works');
