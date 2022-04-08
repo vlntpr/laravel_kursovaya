813fd0fb34e3f22968e87ec8c5d1e8b8f1a2b4e9
vlnt <56691071+vlntpr@users.noreply.github.com>
2022-04-08 13:04:38 +0400

    Add files via upload

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,6 +4,10 @@
 
 @section('content')
 
+<select class="js-example-basic-single" name="state">
+  <option value="AL">Alabama</option>
+  <option value="WY">Wyoming</option>
+</select>
 
 
 @endsection
--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -4,29 +4,33 @@
 
 @section('content')
 <div class="container">
-<div class="works">
-  <div class="wrap-works">
-    <div class="line" style="margin-right:60px; "><h1>Works</h1></div>
-    <div class="line"><form>
-      <div class="input-group w-xl-75 w-xxl-50 d-flex flex-end-center">
-        <input class="form-control rounded-pill border-0 font-base" id="formGroupExampleInput" type="text" placeholder="Search books, articles, keywords"><img class="input-box-icon me-3" src="/img/search.png" width="18" alt="">
+  <div class="works">
+ 
+    <div class="row" style="margin-top:20px;">
+      <div class="col-md-2" style="padding-top:30px; margin-bottom:10px; background-color:white; margin-right: 10px; text-align: left; height:350px;">
+        <h4 style="margin-bottom:20px;">Category</h4>
+        <p><a class="link" href="{{ route('more') }}">Fandom</a>
+        <p><a class="link" href="{{ route('more') }}">Fandom</a>
+        <p><a class="link" href="{{ route('more') }}">Fandom</a>
+        <p><a class="link" href="{{ route('more') }}">Fandom</a>
       </div>
-    </form></div>
-  </div>
-</div> 
-  <div class="row" style="margin-top:20px;">
-    <div class="col-md-2" style="padding-top:30px; margin-bottom:10px; background-color:white; margin-right: 10px; text-align:center; ">
-      <p><a class="link" href="{{ route('more') }}">Fandom</a>
-      <p><a class="link" href="{{ route('more') }}">Fandom</a>
-      <p><a class="link" href="{{ route('more') }}">Fandom</a>
-      <p><a class="link" href="{{ route('more') }}">Fandom</a>
+      <div class="col" style="padding:0;background-color: transparent; " >
+        <div class="line">
+          <h1>Recent works</h1>
+          <h6>tags:</h6> 
+          <div class="mb-3"><select class="js-example-basic-multiple" name="states[]" multiple="multiple" style="width:300px;">
+            <option value="AL">Alabama</option>
+            <option value="WY">Wyoming</option>
+            <option value="WY">Wyoming</option>
+
+          </select></div>
+          <!-- https://select2.org/getting-started/basic-usage -->
+        </div>
+        @include('inc.work')
+        @include('inc.work')
+        </div>
+      </div> 
     </div>
-    <div class="col" style="padding:0;background-color: transparent; " >
-        @include('inc.work')
-        @include('inc.work')
-        @include('inc.work')
-    </div>
-  </div>
 </div>
 
 
